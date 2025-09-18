4db7f22e75ad9ae3dcefaf3aa61498e6860960ab
Ryuinosan <lucas.ryuino03@gmail.com>
2025-09-18 17:23:32 -0300

    Pagina_InicialV0.01

---
+++ AnjunaSite/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import BG_Anjuna from './assets/BG-Anjuna.png'
+
+function App() {
+  return (
+    <>
+      <div>
+       <div className="cbc">
+      <img src={BG_Anjuna} alt="background da anjuna" />
+       </div>
+      
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
